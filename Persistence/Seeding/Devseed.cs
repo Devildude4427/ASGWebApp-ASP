03cb627206a352395218bfde2147153e16f53dd7
Devildude4427 <ryanchristian4427@gmail.com>
2019-02-17 17:32:21 +0000

    Fixing devseed to match changes to user table
    Former-commit-id: 45fa8688fc40538a1ea9418a1aa59c4923b32cc7

--- Persistence/Seeding/Devseed.cs
+++ Persistence/Seeding/Devseed.cs
@@ -12,13 +12,13 @@ namespace Persistence.Seeding
             var connection = container.Resolve<DatabaseConnection>();
 
             await connection.Db.ExecuteAsync(@"
-                INSERT INTO users(first_name, last_name, email, role, password, activated)
-                VALUES('Erwin', 'Schroedinger', 'admin@asg.com', '9001', '$2y$12$sx43vQhbkljyyofORuxz8.4GMc4kysuwroFl8.5pJQGfRhrun1Cwm', true),
-                      ('Johannes', 'Kepler', 'admin2@asg.com', '9001', '$2a$10$cGXMmSfOg2ZrUXUgCxaiaO4pgjJsSvcFYlKPMcFDBgVp3MqrZ2M96', true),
-                      ('Blaise', 'Pascal', 'candidate@asg.com', '1001', '$2a$10$S4MOcUgShERWJpI1EpTReeNbwKL09wElxbCLLimHdf3yrOG7H2PWG', true),
-                      ('Caroline', 'Herschel', 'candidate2@asg.com', '1001', '$2a$10$uzGe/I2v.1LtdTPBQg8oDOe/07mlgaGyG0UoF8G5VQD7b6PAvTQHq', true),
-                      ('Edmond', 'Halley', 'candidate3@asg.com', '1001', '$2a$10$H2GWvDnlRJD1DuVPkfkSLujsmzSwGmM7ELKpZY0olKQFObp4GWZ56', true),
-                      ('Enrico', 'Fermi', 'candidate4@asg.com', '1001', '$2a$10$1XELQEuTrZ0JbFg0MijjI.4QFWJYMppieYp5XN8Aj6z6LIg9QwgN2', true);
+                INSERT INTO users(name, email, role, password, activated)
+                VALUES('Erwin Schroedinger', 'admin@asg.com', '9001', '$2y$12$sx43vQhbkljyyofORuxz8.4GMc4kysuwroFl8.5pJQGfRhrun1Cwm', true),
+                      ('Johannes Kepler', 'admin2@asg.com', '9001', '$2a$10$cGXMmSfOg2ZrUXUgCxaiaO4pgjJsSvcFYlKPMcFDBgVp3MqrZ2M96', true),
+                      ('Blaise Pascal', 'candidate@asg.com', '1001', '$2a$10$S4MOcUgShERWJpI1EpTReeNbwKL09wElxbCLLimHdf3yrOG7H2PWG', true),
+                      ('Caroline Herschel', 'candidate2@asg.com', '1001', '$2a$10$uzGe/I2v.1LtdTPBQg8oDOe/07mlgaGyG0UoF8G5VQD7b6PAvTQHq', true),
+                      ('Edmond Halley', 'candidate3@asg.com', '1001', '$2a$10$H2GWvDnlRJD1DuVPkfkSLujsmzSwGmM7ELKpZY0olKQFObp4GWZ56', true),
+                      ('Enrico Fermi', 'candidate4@asg.com', '1001', '$2a$10$1XELQEuTrZ0JbFg0MijjI.4QFWJYMppieYp5XN8Aj6z6LIg9QwgN2', true);
             ");
         }
     }
