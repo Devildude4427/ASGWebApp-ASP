828dfafd7a9f7b81ccbaedd4ae8dde480fb18e20
Devildude4427 <ryanchristian4427@gmail.com>
2019-02-23 17:43:05 +0000

    Added Enum for storing current stage that the candidate is on

--- Domain/Entities/Candidate.cs
+++ Domain/Entities/Candidate.cs
@@ -7,13 +7,33 @@ namespace Domain.Entities
         public string ReferenceNumber { get; set; }
         public long ContactInfoId { get; set; }
         public long GeneralInfoId { get; set; }
+        public CandidateStage CandidateStage { get; set; }
     }
 
     public class NewCandidate
     {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public UserRole Role = UserRole.Standard;
-        public string PasswordHash { get; set; }
+        public long UserId { get; set; }
+        public string ReferenceNumber { get; set; }
+        public long ContactInfoId { get; set; }
+        public long GeneralInfoId { get; set; }
+        public CandidateStage CandidateStage = 0;
+    }
+    
+    public enum CandidateStage
+    {
+        Registered = 0,
+        Paid = 1,
+        AwaitingCourse = 2,
+        CourseJoiningInstructions = 3,
+        GroundSchoolSyllabus = 4,
+        GroundSchoolExam = 5,
+        ApproveExamResults = 6,
+        ReturnExamResults = 7,
+        GivenOpsManualTemplate = 8,
+        ReturnedOpsManual = 9,
+        InsuranceCheck = 10,
+        FlightAssessment = 11,
+        CaaPfCoRecommendation = 12,
+        CourseReview = 13
     }
 }
