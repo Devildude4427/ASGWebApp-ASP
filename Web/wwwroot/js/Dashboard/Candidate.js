d4f780297d1e278f00f1c4681e7edd52c4c9f1c6
Devildude4427 <ryanchristian4427@gmail.com>
2019-02-24 22:39:03 +0000

    Basics of using Vue and Axios
    Former-commit-id: bcd3b59b5e355844d8a9c5a8041d928ee41e0ee8
    Former-commit-id: 524e4c0e90597d99469d507303c8e8d3000c0071

---
+++ Web/wwwroot/js/Dashboard/Candidate.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#app',
+    data () {
+        return {
+            user: 0
+        }
+    },
+    mounted () {
+        axios
+            .get('/api/v1/user')
+            .then(response => (this.user = response.data));
+    }
+});
