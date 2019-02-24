abe4f68d2c7b909f4c9759b217a615edb29a72af
Devildude4427 <ryanchristian4427@gmail.com>
2019-02-24 01:54:45 +0000

    Cleaning of API controllers. Still largely unsure of how to split up responsibilities, especially when it comes to authority levels. Will figure out later

--- Web/Api/v1/UserApiController.cs
+++ Web/Api/v1/UserApiController.cs
@@ -1,36 +1,32 @@
 using System.Threading.Tasks;
+using Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Services;
 
 namespace Web.Api.v1
 {
-    [Route("/api/v1/users")]
-    // [Authorize("Admin")]
+    [Route("/api/v1/user")]
     public class UserApiController : RootApiController
     {
         private readonly UserService _userService;
 
         private readonly CandidateService _candidateService;
+        
+        private readonly IUserIdentity _user;
 
-        public UserApiController(UserService userService, CandidateService candidateService)
+        public UserApiController(UserService userService, CandidateService candidateService, IUserIdentity user)
         {
             _userService = userService;
             _candidateService = candidateService;
+            _user = user;
         }
 
-        [HttpGet]
-        public async Task<IActionResult> Find()
-        {
-            var filteredPageRequest = Request.FilteredPageRequest("id", true);
-            var result = await _userService.Find(filteredPageRequest);
-            return Json(result);
-        }
-
-        // [HttpGet("{id:long}")]
-        // public async Task<IActionResult> FindById(long id)
+        // [HttpGet]
+        // public async Task<IActionResult> Find()
         // {
-        //     var result = await _userService.FindById(id);
+        //     var filteredPageRequest = Request.FilteredPageRequest("id", true);
+        //     var result = await _userService.Find(filteredPageRequest);
         //     return Json(result);
         // }
         
@@ -38,6 +34,13 @@ namespace Web.Api.v1
         public async Task<IActionResult> FindCandidateByUserId(long id)
         {
             var result = await _candidateService.FindByUserId(id);
+            return Json(result);
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> GetCurrentCandidate()
+        {
+            var result = await _candidateService.FindByUserId(_user.Id);
             return Json(result);
         }
     }
