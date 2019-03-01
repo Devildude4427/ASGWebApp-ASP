665c17d5549e9d95bd45372ffd00e387b4d68896
Devildude4427 <ryanchristian4427@gmail.com>
2019-03-01 00:03:29 +0000

    Expanding upon devseed

--- Persistence/Seeding/Devseed.cs
+++ Persistence/Seeding/Devseed.cs
@@ -21,10 +21,55 @@ namespace Persistence.Seeding
                       ('Enrico Fermi', 'candidate4@asg.com', '1001', '$2y$12$YGA1lrJ7MlhgKtNU0qy4UOtrvlYpswFlMjVrqDdaFQLTS54/MYhyy', true);
 
                 INSERT INTO candidates(id, user_id, reference_number, contact_info_id, general_info_id, last_completed_stage)
-                VALUES(1, 3, 'ASG-001-19-02', 1, 1, -1),
-                      (2, 4, 'ASG-002-19-02', 2, 2, 4),
+                VALUES(2, 4, 'ASG-002-19-02', 2, 2, 4),
                       (3, 5, 'ASG-003-19-02', 3, 3, 7),
                       (4, 6, 'ASG-004-19-02', 4, 4, 12);
+
+                INSERT INTO address(id, line_1, line_2, city, post_code)
+                VALUES(2, 'Unit 14, 3 Edgar Buildings', 'George Street', 'Bath', 'BA1 2FJ'),
+                      (3, 'Box 777, 91 Western Road', 'Brighton', 'East Sussex', 'BN1 2NW'),
+                      (4, 'Room 67, 14 Tottenham Court Road', '', 'London', 'W1T 1JY');
+
+                INSERT INTO contact_information (id, address_id, phone_number)
+                VALUES (2, 2, '7597268597'),
+                       (3, 3, '7911123456'),
+                       (4, 4, '7911564856');
+
+                INSERT INTO drones (make, model)
+                VALUES
+                       ('DJI', 'Matrice'),
+                       ('DJI', 'Mavic'),
+                       ('DJI', 'Mavic 2'),
+                       ('DJI', 'Mavic Air'),
+                       ('DJI', 'Mavic Pro'),
+                       ('DJI', 'Mavic Pro Platinum'),
+                       ('DJI', 'Phantom 1'),
+                       ('DJI', 'Phantom FC40'),
+                       ('DJI', 'Phantom 2'),
+                       ('DJI', 'Phantom 2 Vision'),
+                       ('DJI', 'Phantom 2 Vision+'),
+                       ('DJI', 'Phantom 3 Standard'),
+                       ('DJI', 'Phantom 3 4k'),
+                       ('DJI', 'Phantom 3 SE'),
+                       ('DJI', 'Phantom 3 Advanced'),
+                       ('DJI', 'Phantom 3 Professional'),
+                       ('DJI', 'Phantom 4'),
+                       ('DJI', 'Phantom 4 Advanced'),
+                       ('DJI', 'Phantom 4 Pro'),
+                       ('DJI', 'Phantom 4 Pro V2.0'),
+                       ('DJI', 'Inspire 1'),
+                       ('DJI', 'Inspire 1 Pro/Raw'),
+                       ('DJI', 'Inspire 2'),
+                       ('Parrot', 'Anafi'),
+                       ('Parrot', 'Bebop 2'),
+                       ('Parrot', 'Bebop 2 Power'),
+                       ('Parrot', 'Mambo');
+
+                INSERT INTO general_information(id, english_speaking_level, place_of_birth, date_of_birth, preferred_course_location, drone_id, paid)
+                VALUES (2, 6, 'Somerset', '1990-10-21', 'Somerset', 5, true),
+                       (3, 6, 'Aberdeenshire', '1990-12-31', 'Aberdeenshire', 10, true),
+                       (4, 6, 'Cardiff', '1990-11-15', 'Cardiff', 1, true);
+
             ");
         }
     }
