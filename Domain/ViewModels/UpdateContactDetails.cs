6dd35eaccbd6b9e01776b25a44b5295a4380eb1d
Devildude4427 <ryanchristian4427@gmail.com>
2019-03-10 02:19:59 +0000

    Modifications to 'UpdateContactDetails' page, as well as adding a place for it to post to
    Former-commit-id: f8d51fc2cfdbc09b4da931f2118e49c4e22a4873
    Former-commit-id: 6abda5066bf112cc9bb249e3ac01d1003ce7fe14

---
+++ Domain/ViewModels/UpdateContactDetails.cs
@@ -0,0 +1,12 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.ViewModels
+{
+    public class UpdateContactDetails
+    {
+        public Address Address { get; set; }
+        
+        public string CompanyName { get; set; }
+    }
+}
---
+++ Web/Api/v1/CandidateAccountDataApiController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace Web.Api.v1
+{
+    [Route("/api/v1/candidate")]
+    public class CourseRegistrationApiController : RootApiController
+    {
+
+        private readonly CandidateService _candidateService;
+
+        public CourseRegistrationApiController(CandidateService candidateService)
+        {
+            _candidateService = candidateService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] CourseRegistration courseRegistration)
+        {
+            await _candidateService.Register(courseRegistration);
+            return Ok();
+        }
+        
+        [HttpPost("updateContact")]
+        public async Task<IActionResult> UpdateContact([FromBody] UpdateContactDetails updateContactDetails)
+        {
+            // await _candidateService.Register(courseRegistration);
+            Console.WriteLine(updateContactDetails.CompanyName);
+            return Ok();
+        }
+
+        // [HttpGet("Find")]
+        // public async Task<IActionResult> Find()
+        // {
+        //     var filteredPageRequest = Request.FilteredPageRequest("id", true);
+        //     var result = await _exampleService.Find(filteredPageRequest);
+        //     return Json(result);
+        // }
+    }
+}
