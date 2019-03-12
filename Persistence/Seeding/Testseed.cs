afc412e0cfd450728003cc4da519f7fa89159ab0
Devildude4427 <ryanchristian4427@gmail.com>
2019-03-12 18:35:38 +0000

    Updating TestSeed to match current database structure.

--- Persistence/Seeding/Testseed.cs
+++ Persistence/Seeding/Testseed.cs
@@ -12,8 +12,33 @@ namespace Persistence.Seeding
             var connection = container.Resolve<DatabaseConnection>();
 
             await connection.Db.ExecuteAsync(@"
-                INSERT INTO users(name, email, role, password_hash, activated)
-                VALUES('Admin User', 'admin@example.com', 9001, '$2y$12$sx43vQhbkljyyofORuxz8.4GMc4kysuwroFl8.5pJQGfRhrun1Cwm', true);
+                INSERT INTO users(name, email, role, password, activated)
+                VALUES ('Erwin Schroedinger', 'admin@asg.com', '9001', '$2y$12$IF458i6pDVsY.XJR5OYd.OzjRPaPIsHmQhyE93E2V2SonZszVmzXm', true),
+                       ('Edmond Halley', 'candidate3@asg.com', '1001', '$2y$12$E9LNbS1h2VZ/lErbbWunVOpA67QJrPmPCqCBuGuLKuMY/r.QjEjea', true),
+                       ('Enrico Fermi', 'candidate4@asg.com', '1001', '$2y$12$YGA1lrJ7MlhgKtNU0qy4UOtrvlYpswFlMjVrqDdaFQLTS54/MYhyy', true);
+
+                INSERT INTO addresses(id, line_1, line_2, city, post_code)
+                VALUES (3, 'Box 777, 91 Western Road', 'Brighton', 'East Sussex', 'BN1 2NW'),
+                       (4, 'Room 67, 14 Tottenham Court Road', '', 'London', 'W1T 1JY');
+
+                INSERT INTO contact_information (id, address_id, phone_number)
+                VALUES (3, 3, '7911123456'),
+                       (4, 4, '7911564856');
+
+                INSERT INTO drones (make, model)
+                VALUES ('DJI', 'Matrice'),
+                       ('DJI', 'Mavic'),
+                       ('DJI', 'Mavic 2'),
+                       ('DJI', 'Mavic Air'),
+                       ('DJI', 'Mavic Pro');
+
+                INSERT INTO general_information(id, english_speaking_level, place_of_birth, date_of_birth, preferred_course_location, drone_id, paid)
+                VALUES (3, 6, 'Aberdeenshire', '1990-12-31', 'Aberdeenshire', 10, true),
+                       (4, 6, 'Cardiff', '1990-11-15', 'Cardiff', 1, true);
+
+                INSERT INTO candidates(id, user_id, reference_number, contact_info_id, general_info_id, last_completed_stage)
+                VALUES (3, 5, 'ASG-003-19-02', 3, 3, 7),
+                       (4, 6, 'ASG-004-19-02', 4, 4, 12);
             ");
         }
     }
