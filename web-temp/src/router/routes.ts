4bc05a78fb50c3a59e91385c93c55c41af699dca
Devildude4427 <ryanchristian4427@gmail.com>
2019-03-25 18:05:42 +0000

    Base project with Vue working, though no where near integrated. Webpack still needs to be setup

---
+++ web-temp/src/router/routes.ts
@@ -0,0 +1,47 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import Home from '../views/Home.vue';
+import Login from '../views/Login.vue';
+
+Vue.use(Router);
+
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    },
+    {
+      path: '/404',
+      name: '404',
+      component: require('../views/_404').default,
+      // Allows props to be passed to the 404 page through route
+      // params, such as `resource` to define what wasn't found.
+      props: true,
+    },
+    // Redirect any unmatched routes to the 404 page. This may
+    // require some server configuration to work in production:
+    // https://router.vuejs.org/en/essentials/history-mode.html#example-server-configurations
+    {
+      path: '*',
+      redirect: '404',
+    },
+  ],
+});
+
+export default router;
