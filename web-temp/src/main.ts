a262f8db2469a6efac92102f490bdcf344ecdea5
Devildude4427 <ryanchristian4427@gmail.com>
2019-03-26 23:30:36 +0000

    Working layouts, though I will be scraping bourbon, to instead move on to vuetify
    Former-commit-id: 082a8f7b74782e0c5794a24029fa8ac7f5c3676d
    Former-commit-id: 78ee9baee438eda45fd33ddaa110b477c9ddb0de

--- web-temp/src/main.ts
+++ web-temp/src/main.ts
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router/routes';
 
+import Dashboard from '@/layouts/Dashboard.vue';
+
+Vue.component('dashboard-layout', Dashboard);
+
 Vue.config.productionTip = false;
 
 new Vue({
--- web-temp/src/views/ClientDashboard.vue
+++ web-temp/src/views/ClientDashboard.vue
@@ -1,16 +1,18 @@
 <template>
     <div>
-        <slot></slot>
-        <p>Fuck Bitches Get Money</p>
+        <p>Testing Testing 123</p>
     </div>
 </template>
 
 <script>
+    
+
     export default {
         name: 'ClientDashboard',
     };
 </script>
 
-<style scoped>
-
+<style lang="scss">
+    @import '../sass/_bourbon-neat.scss';
+    @import '../sass/2-base/_base.scss';
 </style>
