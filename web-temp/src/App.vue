264a1c4fc81d685097ac3e4ab736fe00f25467e1
Devildude4427 <ryanchristian4427@gmail.com>
2019-03-27 16:44:14 +0000

    Base layout, though it is too bright as of right now

--- web-temp/src/App.vue
+++ web-temp/src/App.vue
@@ -1,43 +1,20 @@
 <template>
-  <div id="app">
-    <component :is="layout">
-      <router-view/>
-    </component>
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
-    <!-- <router-view/> -->
-  </div>
+    <v-app>
+        <core-filter />
+
+        <core-toolbar />
+
+        <core-drawer />
+
+        <core-view />
+    </v-app>
 </template>
 
-<script>
-export default {
-  computed: {
-    layout() {
-      return (this.$route.meta.layout) + '-layout';
-    },
-  },
-};
-</script>
-
-
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+    @import 'sass/material-dashboard';
+
+    /* Remove in 1.2 */
+    .v-datatable thead th.column.sortable i {
+        vertical-align: unset;
     }
-  }
-}
 </style>
