d8605d9f228b0949376d604e9f4d7ec67e0835d4
Devildude4427 <ryanchristian4427@gmail.com>
2019-03-27 23:44:10 +0000

    Redid the routing system
    Former-commit-id: 69ef10f33921ea99b444fb3ce464b28796ac5dba
    Former-commit-id: 324fd8e1459a586c196599904836d21db78780aa

--- web-temp/src/router/routes.ts
+++ web-temp/src/router/routes.ts
@@ -1,75 +1,38 @@
-import Vue from 'vue';
-import VueAnalytics from 'vue-analytics';
-import Router from 'vue-router';
-import Meta from 'vue-meta';
-
-import Dashboard from '@/views/Dashboard.vue';
-import UserProfile from '@/views/UserProfile.vue';
-import TableList from '@/views/TableList.vue';
-import Typography from '@/views/Typography.vue';
-import Icons from '@/views/Icons.vue';
-import Maps from '@/views/Maps.vue';
-import Notifications from '@/views/Notifications.vue';
-import Upgrade from '@/views/Upgrade.vue';
-
-
-Vue.use(Router);
-
-const router = new Router({
-  mode: 'history',
-  routes: [
+export default [
     {
-      path: '/dashboard',
-      // Relative to /src/views
-      component: Dashboard,
+        path: '/dashboard',
+        // Relative to /src/views
+        view: 'Dashboard',
     },
     {
-      path: '/user-profile',
-      name: 'User Profile',
-      component: UserProfile,
+        path: '/user-profile',
+        name: 'User Profile',
+        view: 'UserProfile',
     },
     {
-      path: '/table-list',
-      name: 'Table List',
-      component: TableList,
+        path: '/table-list',
+        name: 'Table List',
+        view: 'TableList',
     },
     {
-      path: '/typography',
-      component: Typography,
+        path: '/typography',
+        view: 'Typography',
     },
     {
-      path: '/icons',
-      component: Icons,
+        path: '/icons',
+        view: 'Icons',
     },
     {
-      path: '/maps',
-      component: Maps,
+        path: '/maps',
+        view: 'Maps',
     },
     {
-      path: '/notifications',
-      component: Notifications,
+        path: '/notifications',
+        view: 'Notifications',
     },
     {
-      path: '/upgrade',
-      name: 'Upgrade to PRO',
-      component: Upgrade,
+        path: '/upgrade',
+        name: 'Upgrade to PRO',
+        view: 'Upgrade',
     },
-  ],
-});
-
-Vue.use(Meta);
-
-// Bootstrap Analytics
-// Set in .env
-// https://github.com/MatteoGabriele/vue-analytics
-if (process.env.GOOGLE_ANALYTICS) {
-  Vue.use(VueAnalytics, {
-    id: process.env.GOOGLE_ANALYTICS,
-    router,
-    autoTracking: {
-      page: process.env.NODE_ENV !== 'development',
-    },
-  });
-}
-
-export default router;
+];
