084836587445ea6c0c3cfc7c44a0aa27f894a91a
Devildude4427 <ryanchristian4427@gmail.com>
2019-04-01 23:17:43 +0100

    Removing duplicate directory of old, split vue
    Former-commit-id: 37f15917cafbdcfec714d13bed12159f0b35e4b3
    Former-commit-id: c2f3807014149bb712348da028a5bc5976e4c0c2

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,36 +1,66 @@
+using System;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Domain.Entities;
 using Domain.RepositoryInterfaces;
 using Domain.ViewModels;
+using Microsoft.Extensions.Options;
+using Services.Helpers;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Services
 {
-    public class AccountService
+    public class AuthService
     {
         private readonly IUserRepository _userRepository;
+        
+        private readonly AppSettings _appSettings;
 
-        public AccountService(IUserRepository userRepository)
+        public AuthService(IUserRepository userRepository, IOptions<AppSettings> appSettings)
         {
             _userRepository = userRepository;
+            _appSettings = appSettings.Value;
         }
 
-        public async Task<UserResponse> Login(string email, string password) => await GetUserIfValid(email, password);
-
-        private async Task<UserResponse> GetUserIfValid(string email, string password)
+        public async Task<UserResponse> Login(string email, string password)
         {
-            var user = await _userRepository.FindByEmail(email);
+            var user = await GetUserIfValid(email);
+
             if (user == null)
                 return new UserResponse(LoginResponse.UserNonExistent);
             
+            CreateAuthToken(user);
+
             if (!user.Activated)
                 return new UserResponse(user, LoginResponse.UserNotActivated);
 
             var hashedPassword = await _userRepository.GetHashedPassword(email);
             if (!Hashing.PasswordsMatch(password, hashedPassword))
                 return new UserResponse(user, LoginResponse.IncorrectPassword);
-            
+
             return !user.Enabled ? new UserResponse(user, LoginResponse.UserDisabled) :
                 new UserResponse(user, LoginResponse.Successful);
+        }
+        
+        private async Task<User> GetUserIfValid(string email) => await _userRepository.FindByEmail(email);
+
+        private void CreateAuthToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] 
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            user.Token = tokenHandler.WriteToken(token);
         }
 
         public async Task<UserResponse> Register(RegistrationRequest registrationRequest) =>
--- web-temp/src/utils/vuex.ts
+++
@@ -1,5 +0,0 @@
-export const set = (property: string | number) =>
-    (state: { [x: string]: any; }, payload: any) => (state[property] = payload);
-
-export const toggle = (property: string | number) =>
-    (state: { [x: string]: any; }) => (state[property] = !state[property]);
