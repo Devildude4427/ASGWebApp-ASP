07610d16b0aa64211b28f9a4369be3a164ae6917
Devildude4427 <ryanchristian4427@gmail.com>
2019-04-02 15:39:04 +0100

    Added 'UpdateContactDetails' form with a route
    Former-commit-id: 6a80934f12fc4846a3eede4af1ac5e9d3602f301
    Former-commit-id: 4d30d811cfe96d02c59dd184456a40861e301fde

---
+++ Web/Vue/src/views/UpdateContactDetails.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container fill-height fluid grid-list-xl>
+    <v-layout justify-center wrap>
+      <v-flex xs12 md8>
+        <material-card color="blue" title="Update Contact Details" text="So that we can remain in contact">
+          <v-form>
+            <v-container py-0>
+              <v-layout wrap>
+                <v-flex xs12 md6>
+                  <v-text-field label="Address Line 1" class="info-input"/>
+                </v-flex>
+                <v-flex xs12 md6> 
+                  <v-text-field label="Address Line 2" class="info-input"/>
+                </v-flex>
+                <v-flex xs12 md6>
+                  <v-text-field label="City" class="info-input"/>
+                </v-flex>
+                <v-flex xs12 md6>
+                  <v-text-field label="Postal Code" class="info-input"/>
+                </v-flex>
+                <v-flex xs12 md12>
+                  <v-spacer></v-spacer>
+                </v-flex>
+                <v-flex xs12 md6>
+                  <v-text-field label="Phone Number" class="info-input"/>
+                </v-flex>
+                <v-flex xs12 md6>
+                  <v-text-field label="Email Address" class="info-input"/>
+                </v-flex>
+                <v-flex xs12 text-xs-right>
+                  <v-btn class="mx-0 font-weight-light" color="blue">Update Profile</v-btn>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-form>
+        </material-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  //
+};
+</script>
