522c5ea40bfbc7289f0fa30a5278d82ff1a7c20a
Devildude4427 <ryanchristian4427@gmail.com>
2019-04-27 23:50:00 +0100

    Finally wrapped my head around JWT and how authentication should be handled. Minor changes to cement in this understanding.
    Former-commit-id: f32719618bad248d747aa4e402bb9af5f5f78e29
    Former-commit-id: 06f484f9999aaf8b17111cd7af0cc63311ebd133

--- Web/Vue/src/views/UpdateContactDetails.vue
+++ Web/Vue/src/views/UpdateContactDetails.vue
@@ -3,32 +3,32 @@
     <v-layout justify-center wrap>
       <v-flex xs12 md8>
         <material-card color="blue" title="Update Contact Details" text="So that we can remain in contact">
-          <v-form>
+          <v-form @submit.prevent="handleSubmit">
             <v-container py-0>
               <v-layout wrap>
                 <v-flex xs12 md6>
-                  <v-text-field label="Address Line 1" class="info-input"/>
+                  <v-text-field label="Address Line 1" class="info-input"></v-text-field>
                 </v-flex>
                 <v-flex xs12 md6> 
-                  <v-text-field label="Address Line 2" class="info-input"/>
+                  <v-text-field label="Address Line 2" class="info-input"></v-text-field>
                 </v-flex>
                 <v-flex xs12 md6>
-                  <v-text-field label="City" class="info-input"/>
+                  <v-text-field label="City" class="info-input"></v-text-field>
                 </v-flex>
                 <v-flex xs12 md6>
-                  <v-text-field label="Postal Code" class="info-input"/>
+                  <v-text-field label="Postal Code" class="info-input"></v-text-field>
                 </v-flex>
                 <v-flex xs12 md12>
                   <v-spacer></v-spacer>
                 </v-flex>
                 <v-flex xs12 md6>
-                  <v-text-field label="Phone Number" class="info-input"/>
+                  <v-text-field label="Phone Number" class="info-input"></v-text-field>
                 </v-flex>
                 <v-flex xs12 md6>
-                  <v-text-field label="Email Address" class="info-input"/>
+                  <v-text-field label="Email Address" class="info-input"></v-text-field>
                 </v-flex>
                 <v-flex xs12 text-xs-right>
-                  <v-btn class="mx-0 font-weight-light" color="blue">Update Profile</v-btn>
+                  <v-btn class="mx-0 font-weight-light" type="submit" color="blue">Update Profile</v-btn>
                 </v-flex>
               </v-layout>
             </v-container>
@@ -40,7 +40,20 @@
 </template>
 
 <script>
+  const test = "test";
 export default {
-  //
+  methods: {
+    handleSubmit(e) {
+      this.$http.post('test/test', test)
+        .then((response) => {
+          console.log(response);
+          // if (response.statusText === 'OK') {
+          //   window.location.href = `/dashboard`;
+          // }
+        }).catch((error) => {
+        console.log(error);
+        });
+    },
+  },
 };
 </script>
