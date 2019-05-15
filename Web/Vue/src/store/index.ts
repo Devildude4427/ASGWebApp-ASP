fba6bce839f63596a5fe9108f527c598d81949a9
Devildude4427 <ryanchristian4427@gmail.com>
2019-05-15 19:03:14 +0100

    Started to migrate to using vuex more heavily in place of separate API calls.
    Former-commit-id: 377516b9d42b9bf944b7ef1cd998d8a9da74d52c
    Former-commit-id: 5c6327a33f6581a0991fad0107092c169ef6c963

--- Web/Vue/src/store/index.ts
+++ Web/Vue/src/store/index.ts
@@ -3,7 +3,7 @@ import Vuex from 'vuex';
 
 import { alert } from '@/store/alert-module';
 import { authentication } from '@/store/authentication-module';
-import { users } from '@/store/users-module';
+import users from '@/store/users-module';
 import { app } from '@/store/app-module';
 
 Vue.use(Vuex);
