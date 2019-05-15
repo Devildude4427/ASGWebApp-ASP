948a88f4e5f4b5904898a3a309051b3c96f4ee6a
Devildude4427 <ryanchristian4427@gmail.com>
2019-05-15 16:38:06 +0100

    Fixed issue involving the wrong background color for data tables.
    Former-commit-id: 714fd763cd12b82d6f8a1febf84d356bf056af4c
    Former-commit-id: cbf00e46a076d78ce3fe2b6e5b319c24d97c5741

--- Web/Vue/src/views/TableList.vue
+++ Web/Vue/src/views/TableList.vue
@@ -1,39 +1,13 @@
 <template>
-  <v-container
-    fill-height
-    fluid
-    grid-list-xl
-  >
-    <v-layout
-      justify-center
-      wrap
-    >
-      <v-flex
-        md12
-      >
-        <material-card
-          color="green"
-          title="Simple Table"
-          text="Here is a subtitle for this table"
-        >
-          <v-data-table
-            :headers="headers"
-            :items="items"
-            hide-actions
-          >
-            <template
-              slot="headerCell"
-              slot-scope="{ header }"
-            >
-              <span
-                class="subheading font-weight-light text-success text--darken-3"
-                v-text="header.text"
-              />
+  <v-container fill-height fluid grid-list-xl>
+    <v-layout justify-center wrap>
+      <v-flex md12>
+        <material-card color="green" title="Simple Table" text="Here is a subtitle for this table">
+          <v-data-table :headers="headers" :items="items" hide-actions>
+            <template slot="headerCell" slot-scope="{ header }">
+              <span class="subheading font-weight-light text-success text--darken-3" v-text="header.text"></span>
             </template>
-            <template
-              slot="items"
-              slot-scope="{ item }"
-            >
+            <template slot="items" slot-scope="{ item }">
               <td>{{ item.name }}</td>
               <td>{{ item.country }}</td>
               <td>{{ item.city }}</td>
@@ -42,34 +16,14 @@
           </v-data-table>
         </material-card>
       </v-flex>
-      <v-flex
-        md12
-      >
-        <material-card
-          color="green"
-          flat
-          full-width
-          title="Table on Plain Background"
-          text="Here is a subtitle for this table"
-        >
-          <v-data-table
-            :headers="headers"
-            :items="items.slice(0, 7)"
-            hide-actions
-          >
-            <template
-              slot="headerCell"
-              slot-scope="{ header }"
-            >
-              <span
-                class="subheading font-weight-light text--darken-3"
-                v-text="header.text"
-              />
+      <v-flex md12>
+        <material-card color="green" flat full-width title="Table on Plain Background"
+                       text="Here is a subtitle for this table">
+          <v-data-table :headers="headers" :items="items.slice(0, 7)" hide-actions>
+            <template slot="headerCell" slot-scope="{ header }">
+              <span class="subheading font-weight-light text--darken-3" v-text="header.text"></span>
             </template>
-            <template
-              slot="items"
-              slot-scope="{ item }"
-            >
+            <template slot="items" slot-scope="{ item }">
               <td>{{ item.name }}</td>
               <td>{{ item.country }}</td>
               <td>{{ item.city }}</td>
