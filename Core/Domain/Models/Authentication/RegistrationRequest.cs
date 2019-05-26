3d4f624ffb3eecf121944174e849ea5bcc341e2d
Ryan Christian <ryanchristian4427@gmail.com>
2019-05-25 20:53:47 -0500

    Created a handful of new tests to ensure the services are still working properly
    Former-commit-id: 7c796fd9b7149c000d5bb6cc5a9d92d7f6f6fd3b
    Former-commit-id: 573401f07ccc547f2f0e24416175db93a1815514

--- Core/Domain/Models/Authentication/RegistrationRequest.cs
+++ Core/Domain/Models/Authentication/RegistrationRequest.cs
@@ -2,6 +2,13 @@ namespace Core.Domain.Models.Authentication
 {
     public class RegistrationRequest
     {
+        public RegistrationRequest(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+        
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
--- Testing/Integration/UserTests.cs
+++ Testing/Integration/UserTests.cs
@@ -1,22 +1,37 @@
 using System.Threading.Tasks;
 using Autofac;
+using Core.Services;
 using FluentAssertions;
 using Testing.Config;
 using Xunit;
 
 namespace Testing.Integration
 {
+    [Collection("Server")]
     public class UserTests : Test
     {
-        // [Fact]
-        // public async Task CanFindById()
-        // {
-        //     using (var container = GetContainer())
-        //     {
-        //         var service = container.Resolve<ExampleService>();
-        //         var exampleEntity = await service.FindById(1);
-        //         exampleEntity.Name.Should().Be("Test Example");
-        //     }
-        // }
+        [Fact]
+        public async Task CanFindById()
+        {
+            using (var container = GetContainer())
+            {
+                var userService = container.Resolve<IUserService>();
+                var user = await userService.FindById(1);
+                user.Name.Should().Be("Erwin Schroedinger");
+                user.Role.Should().Be(9001);
+            }
+        }
+        
+        [Fact]
+        public async Task CanFindByEmail()
+        {
+            using (var container = GetContainer())
+            {
+                var userService = container.Resolve<IUserService>();
+                var user = await userService.FindByEmail("admin@asg.com");
+                user.Name.Should().Be("Erwin Schroedinger");
+                user.Role.Should().Be(9001);
+            }
+        }
     }
 }
